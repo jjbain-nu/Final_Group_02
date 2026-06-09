1bd2d8846f38fa01c756c427e89f019c51a928e0
Anish-Na <nakka.an@northeastern.edu>
2026-06-09 16:05:08 -0500

    "Person and PersonDirectory set up. Basic human records and a directory to hold them. May change later."

---
+++ ProfileWorkareaExample 3/ProfileWorkareaExample 3/ProfileWorkareaExample/src/Business/Person/Person.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Person;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class Person {
+    //fields
+    String id;
+    String name;
+    String email;
+    String phoneNumber;
+    //constructor
+    public Person(String id) {
+
+        this.id = id;
+        this.name = id;
+    }
+
+    public String getPersonId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+
+    public boolean isMatch(String id) {
+        if (getPersonId().equals(id)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getPersonId();
+    }
+}
