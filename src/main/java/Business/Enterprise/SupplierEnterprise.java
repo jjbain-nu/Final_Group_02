ea7358e57fe5c26fc6d1afd03cc4d6ff858acc41
Yushin Otani <otani.y@northeastern.edu>
2026-07-19 09:38:44 +0900

    Create SupplierEnterprise

---
+++ src/main/java/Business/Enterprise/SupplierEnterprise.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yu101
+ */
+public class SupplierEnterprise extends Enterprise{
+    
+    public SupplierEnterprise(String name){
+        super(name,EnterpriseType.Supplier);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
