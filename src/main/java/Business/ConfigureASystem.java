875d6f15431f60f3063f50eb018ccb83097408b1
Yushin Otani <otani.y@northeastern.edu>
2026-07-19 12:34:01 +0900

    Modified configureASystem

--- src/main/java/Business/ConfigureASystem.java
+++ src/main/java/Business/ConfigureASystem.java
@@ -1,7 +1,12 @@
 package Business;
 
 import Business.Employee.Employee;
+import Business.Enterprise.SupplierEnterprise;
+import Business.Network.Network;
+import Business.Organization.SupplierOrganization;
 import Business.Role.LabManagerRole;
+import Business.Role.MaterialShippingRole;
+import Business.Role.SupplierAdminRole;
 import Business.Role.SystemAdminRole;
 import Business.UserAccount.UserAccount;
 
@@ -16,11 +21,25 @@ public class ConfigureASystem {
         EcoSystem system = EcoSystem.getInstance();
         
         //Create a network
-        //create an enterprise
-        //initialize some organizations
-        //have some employees 
-        //create user account
+        Network network = system.createAndAddNetwork();
+        network.setName("Network");
         
+        //create an enterprise
+        SupplierEnterprise supplierEnterprise = new SupplierEnterprise("Supplier");
+        
+        //initialize some organizations
+        SupplierOrganization supplierOrg = new SupplierOrganization();
+        supplierEnterprise.getOrganizationDirectory().getOrganizationList().add(supplierOrg);
+        network.getEnterpriseDirectory().getEnterpriseList().add(supplierEnterprise);
+        
+        //have some employees 
+        Employee supplierAdminEmp01 = supplierOrg.getEmployeeDirectory().createEmployee("SupplierAdmin01");
+        Employee MaterialShipping01 = supplierOrg.getEmployeeDirectory().createEmployee("MaterialShipping01");
+        
+        //create user account
+        supplierOrg.getUserAccountDirectory().createUserAccount("supplierAdmin","****",supplierAdminEmp01, new SupplierAdminRole());
+        supplierOrg.getUserAccountDirectory().createUserAccount("MaterialShipping","****",MaterialShipping01, new MaterialShippingRole());        
+
         
         Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
         
