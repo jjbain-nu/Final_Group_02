fc6ba39a0421bc2c4e8b303501964417aa0e5f75
Joseph Bain <jjbain@bu.edu>
2026-07-22 21:10:47 -0400

    added shipping order staff screen

--- src/main/java/Business/Role/ShippingOrderStaffRole.java
+++ src/main/java/Business/Role/ShippingOrderStaffRole.java
@@ -5,7 +5,7 @@ import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
-import ui.WholesalerRole.WholesalerWorkAreaJPanel;
+import ui.WholesalerRole.ShippingOrderStaffWorkAreaJPanel;
 
 /**
  * Role responsible for preparing wholesaler shipping orders.
@@ -15,7 +15,7 @@ public class ShippingOrderStaffRole extends Role {
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account,
             Organization organization, Enterprise enterprise, EcoSystem business) {
-        return new WholesalerWorkAreaJPanel(account, enterprise, "Shipping Order Staff Work Area");
+        return new ShippingOrderStaffWorkAreaJPanel(enterprise);
     }
 
     @Override
