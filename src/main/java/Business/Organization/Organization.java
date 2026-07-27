47d93fdb7be10dade4287e64cbb3126bfab6586f
Joseph Bain <jjbain@bu.edu>
2026-07-27 19:58:27 -0400

    Merge branch 'main' into joseph-wholesale

--- src/main/java/Business/Organization/Organization.java
+++ src/main/java/Business/Organization/Organization.java
@@ -24,7 +24,13 @@ public abstract class Organization {
     private static int counter=0;
     
     public enum Type{
-        Admin("Admin Organization"), Doctor("Doctor Organization"), Lab("Lab Organization"), Transport("Transport Organization");
+        Admin("Admin Organization"),
+        Doctor("Doctor Organization"),
+        Lab("Lab Organization"),
+        Supplier("Supplier Organization"),
+        Inventory("Wholesaler Inventory Organization"),
+        Shipping("Wholesaler Shipping Organization"),
+        Transport("Transport Organization");
         private String value;
         private Type(String value) {
             this.value = value;
